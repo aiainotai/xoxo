edf0a41452bbc9b2375447f2294126a04b96a7cf
Syed M Afiq <153475743+aiainotai@users.noreply.github.com>
2025-04-13 23:39:03 +0800

    dd

--- src/ooxx/views.py
+++ src/ooxx/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404,redirect
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'frontend/based/main.html')
--- src/xxoo/urls.py
+++ src/xxoo/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from ooxx import views as frnview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', frnview.index, name='index'),
 ]
 
 
